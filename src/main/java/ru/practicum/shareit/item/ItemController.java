0fa2b23cc34f6f241dea69b9d1254b88027b6025
Julia3202 <redkina_julia7@mail.ru>
2023-10-07 16:15:27 +0300

    Исправила замечания ревьюера

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/items")
 public class ItemController {
     private final ItemService itemService;
-    private final static String USER_ID = "X-Sharer-User-Id";
+    private static final String USER_ID = "X-Sharer-User-Id";
 
     @PostMapping
     public ItemDto create(@RequestHeader(USER_ID) long userId, @RequestBody ItemDto itemDto) {
