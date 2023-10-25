fb00ef5389e6f34c51b51d81b89b6874b115e211
Julia3202 <redkina_julia7@mail.ru>
2023-10-26 01:45:07 +0300

    add-booking

--- src/main/java/ru/practicum/shareit/request/dao/RequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/dao/RequestRepository.java
@@ -1,8 +1,7 @@
 package ru.practicum.shareit.request.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.request.model.Request;
-@Component
+
 public interface RequestRepository extends JpaRepository<Request, Long> {
 }
