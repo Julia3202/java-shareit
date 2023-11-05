d933d9fc1f8f1168451d1eaa599c2c79df5a3578
Julia3202 <redkina_julia7@mail.ru>
2023-11-05 19:39:37 +0300

    исправила замечания

--- src/main/java/ru/practicum/shareit/request/RequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/RequestServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
