f4b3d5d907a01dba5678cd230e91add6392315f5
Julia3202 <redkina_julia7@mail.ru>
2023-11-14 01:38:02 +0300

    add-docker

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
