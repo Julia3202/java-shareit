4cb2176e32ba64936abb216517e085e7c5a78304
Julia3202 <redkina_julia7@mail.ru>
2023-11-14 21:10:26 +0300

    add-docker

--- server/src/test/java/ru/practicum/shareit/request/RequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/request/RequestControllerTest.java
@@ -37,7 +37,7 @@ class RequestControllerTest {
     private ObjectMapper objectMapper;
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     public static final String USER_ID = "X-Sharer-User-Id";
     private RequestDto requestDto;
